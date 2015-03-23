939586c0f6434f8dcf7b409b4a6d89218a2a75f0
Alex Kaufman <alexkaufman06@gmail.com>
2015-03-23 10:45:33 -0700

    Add flash messages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,8 +14,10 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(params[:question])
     if @question.save
+      flash[:success] = "Question Successfully Added!"
       redirect_to questions_path
     else
+      flash[:danger] = "There was a problem creating your question, please try again."
       render :new
     end
   end
@@ -27,9 +29,18 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find(params[:id])
     if @question.update(params[:question])
+      flash[:success] = "Question Successfully Updated!"
       redirect_to questions_path
     else
+      flash[:danger] = "There was a problem updating your question, please try again."
       render :edit
     end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    flash[:danger] = "Post Successfully Deleted!"
+    redirect_to questions_path
   end
 end
